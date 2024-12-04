cfeee37c4888ed95ed5efd4cb0605114ef25c38d
Sujeet Sharma <sksharmacs2005@gmail.com>
2024-12-04 08:11:31 +0530

    updated file

--- src/main/java/com/primary/qualifier/controller/UniversityController.java
+++ src/main/java/com/primary/qualifier/controller/UniversityController.java
@@ -4,7 +4,6 @@ import com.primary.qualifier.inter.University;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
